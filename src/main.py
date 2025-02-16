6e4adabc5e74016ae8b6e06d3f58679126757a29
Joe Vogel <joejvogel@gmail.com>
2025-02-16 19:32:44 -0300

    Limpeza de variaveis não utilizadas

--- src/main.py
+++ src/main.py
@@ -15,10 +15,6 @@ from optimization.nsgaii            import NSGAII
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-
-    alpha_values = [1, 3, 5, 7]  # Angulo de ataque
-    D_values = [0.2, 0.25, 0.3]  # Diâmetros da hélice (m)
-    B_values = [2, 3]  # Número de pás
     
     # Initialize evaluation methods
     aerial_evaluator = AerialBEMT()
